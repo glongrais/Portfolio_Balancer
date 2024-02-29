63a51eb26f8213531f8fa5144783bf3301ba5455
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-02-29 20:02:38 +0100

    Fix tests

--- portfolio_balancer/file_loader.py
+++ portfolio_balancer/file_loader.py
@@ -2,6 +2,12 @@ from portfolio_balancer.stock import Stock
 import json
 
 def load_file(filename: str) -> list[Stock]:
-    f = open(filename)
-    data = json.load(f)
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"The file {filename} was not found.") from e
+    except json.JSONDecodeError as e:
+        raise json.JSONDecodeError(f"Error decoding JSON from the file {filename}.", e.doc, e.pos)
+
     return [Stock(**i) for i in data]
--- tests/file_loader_tests.py
+++ tests/file_loader_tests.py
@@ -5,13 +5,14 @@ from portfolio_balancer.stock import Stock
 from portfolio_balancer.file_loader import load_file  # Replace 'your_module' with the name of your module
 
 def test_load_valid_file():
-    valid_data = [{'name': 'ABC', 'price': 100}, {'name': 'XYZ', 'price': 200}]
+    valid_data = [{'symbol': 'ABC', 'quantity': 100, 'target': 80.0}, {'symbol': 'XYZ', 'quantity': 200, 'target': 20.0}]
     with mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(valid_data))):
         result = load_file('dummy_file.json')
         assert len(result) == 2
         assert all(isinstance(stock, Stock) for stock in result)
-        assert result[0].name == 'ABC'
-        assert result[1].price == 200
+        assert result[0].symbol == 'ABC'
+        assert result[1].quantity == 200
+        assert result[1].target == 20.0
 
 def test_load_invalid_json():
     with mock.patch('builtins.open', mock.mock_open(read_data='invalid json')):
@@ -23,7 +24,7 @@ def test_file_not_found():
         load_file('non_existent_file.json')
 
 def test_empty_file():
-    with mock.patch('builtins.open', mock.mock_open(read_data='')):
+    with mock.patch('builtins.open', mock.mock_open(read_data='[]')):
         result = load_file('dummy_file.json')
         assert result == []
 
