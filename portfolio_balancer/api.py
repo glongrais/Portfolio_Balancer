503b088582fabe4ecb41d924d9af3502a826c99b
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-03-10 10:43:13 +0100

    get price if "currentPrice" not present

--- portfolio_balancer/api.py
+++ portfolio_balancer/api.py
@@ -7,8 +7,12 @@ session.headers['User-agent'] = 'my-program/1.0'
 
 def _load_share(stock: Stock):
     ticker = yf.Ticker(stock.symbol, session=session)
+    print(stock.symbol, "currentPrice" in ticker.info.keys())
+    if "currentPrice" in ticker.info.keys():
+        stock.price = ticker.info["currentPrice"]
+    else:
+        stock.price = ticker.info["previousClose"]
     stock.name = ticker.info["longName"]
-    stock.price = ticker.info["currentPrice"]
     return stock
 
 
