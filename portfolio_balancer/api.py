569a67b21b2acce7ce1a7a5e22442c975d1d4146
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-03-23 15:24:37 +0100

    Removed print statement

--- portfolio_balancer/api.py
+++ portfolio_balancer/api.py
@@ -7,7 +7,6 @@ session.headers['User-agent'] = 'my-program/1.0'
 
 def _load_share(stock: Stock):
     ticker = yf.Ticker(stock.symbol, session=session)
-    print(stock.symbol, "currentPrice" in ticker.info.keys())
     if "currentPrice" in ticker.info.keys():
         stock.price = ticker.info["currentPrice"]
     else:
