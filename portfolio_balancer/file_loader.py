0f747a1f23a1b84cf6faa82c5da3e4c13950786d
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-04-06 17:10:40 +0200

    Load portfolio from Numbers file

--- portfolio_balancer/file_loader.py
+++ portfolio_balancer/file_loader.py
@@ -1,12 +1,15 @@
 from portfolio_balancer.stock import Stock
 from portfolio_balancer.exception import UnsupportedFileTypeError
 import os
+from numbers_parser import Document, EmptyCell
 import json
 
 def load_file(filename: str) -> list[Stock]:
     try:
         if filename.lower().endswith('.json'):
             return _load_json(filename)
+        elif filename.lower().endswith('.numbers'):
+            return _load_numbers(filename)
         else:
             _, file_extension = os.path.splitext(filename)
             raise UnsupportedFileTypeError(file_extension)
@@ -23,3 +26,19 @@ def _load_json(filename: str) -> list[Stock]:
         raise json.JSONDecodeError(f"Error decoding JSON from the file {filename}.", e.doc, e.pos)
 
     return [Stock(**i) for i in data]
+
+def _load_numbers(filename: str) -> list[Stock]:
+    try:
+        doc = Document(filename)
+    except Exception as e:
+        raise e
+    table = doc.sheets['Dividends'].tables['Repartition']
+    table.delete_row(num_rows=table.num_header_rows, start_row=0)
+
+    data = []
+    for row in table.rows(values_only=True):
+        if row[0] is None:
+            continue
+        data.append(Stock(symbol=row[0], quantity=int(row[2]), distribution_target=row[-1]*100))
+
+    return data
