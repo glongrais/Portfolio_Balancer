a0a75f994814d90281a76cffe6d6bbbc36efea2c
guillaume <guillaume.longrais@orange.fr>
2024-04-15 22:48:00 +0200

    Separating numbers import files

--- portfolio_balancer/file_loader.py
+++ portfolio_balancer/file_loader.py
@@ -1,7 +1,7 @@
 from portfolio_balancer.stock import Stock
 from portfolio_balancer.exception import UnsupportedFileTypeError
+from portfolio_balancer.file_loader_numbers import load_numbers
 import os
-from numbers_parser import Document, EmptyCell
 import json
 
 def load_file(filename: str) -> list[Stock]:
@@ -9,7 +9,7 @@ def load_file(filename: str) -> list[Stock]:
         if filename.lower().endswith('.json'):
             return _load_json(filename)
         elif filename.lower().endswith('.numbers'):
-            return _load_numbers(filename)
+            return load_numbers(filename)
         else:
             _, file_extension = os.path.splitext(filename)
             raise UnsupportedFileTypeError(file_extension)
@@ -26,28 +26,3 @@ def _load_json(filename: str) -> list[Stock]:
         raise json.JSONDecodeError(f"Error decoding JSON from the file {filename}.", e.doc, e.pos)
 
     return [Stock(**i) for i in data]
-
-def _load_numbers(filename: str) -> list[Stock]:
-
-    # Value to adapt 
-    SHEET = 'Dividends'
-    TABLE = 'Repartition'
-    SYMBOL = 0
-    QUANTITY = 2
-    DISTRIBUTION_TARGET = -1
-    
-    try:
-        doc = Document(filename)
-    except Exception as e:
-        raise e
-    
-    table = doc.sheets[SHEET].tables[TABLE]
-    table.delete_row(num_rows=table.num_header_rows, start_row=0)
-
-    data = []
-    for row in table.rows(values_only=True):
-        if row[0] is None:
-            continue
-        data.append(Stock(symbol=row[SYMBOL], quantity=int(row[QUANTITY]), distribution_target=row[DISTRIBUTION_TARGET]*100))
-
-    return data
---
+++ portfolio_balancer/file_loader_numbers.py
@@ -0,0 +1,27 @@
+from numbers_parser import Document, EmptyCell
+from portfolio_balancer.stock import Stock
+
+def load_numbers(filename: str) -> list[Stock]:
+
+    # Value to adapt 
+    SHEET = 'Dividends'
+    TABLE = 'Repartition'
+    SYMBOL = 0
+    QUANTITY = 2
+    DISTRIBUTION_TARGET = -1
+    
+    try:
+        doc = Document(filename)
+    except Exception as e:
+        raise e
+    
+    table = doc.sheets[SHEET].tables[TABLE]
+    table.delete_row(num_rows=table.num_header_rows, start_row=0)
+
+    data = []
+    for row in table.rows(values_only=True):
+        if row[0] is None:
+            continue
+        data.append(Stock(symbol=row[SYMBOL], quantity=int(row[QUANTITY]), distribution_target=row[DISTRIBUTION_TARGET]*100))
+
+    return data
