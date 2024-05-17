6e4a7581f9d6b744c669f607cb7c32a13aec926b
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-05-17 14:26:29 +0200

    Moved balancer to static class

--- portfolio_balancer/balancer.py
+++ portfolio_balancer/balancer.py
@@ -4,24 +4,24 @@ from api.api import API
 from portfolio_balancer.stock import Stock
 from portfolio_balancer.file_loader import FileLoader
 
-def _total_value(stocks: list[Stock]) -> float:
+def total_value(stocks: list[Stock]) -> float:
     result = 0.0
     for stock in stocks:
         result += stock.price * stock.quantity
     return result
 
-def _real_distribution(stocks: list[Stock], total_value: float) -> list[Stock]:
+def real_distribution(stocks: list[Stock], total_value: float) -> list[Stock]:
     for stock in stocks:
         stock.distribution_real = round((stock.price*stock.quantity)/total_value*100, 2)
     return stocks
 
 # Rank the stocks by difference between the distribution_target and distribution_real
-def _stocks_ranking(stocks: list[Stock]) -> list[Stock]:
+def stocks_ranking(stocks: list[Stock]) -> list[Stock]:
     result = sorted(stocks, key=lambda stock: (stock.distribution_target - stock.distribution_real))
     result.reverse()
     return result
 
-def _numbers_shares_to_buy(stocks: list[Stock], total_value: float, amount: int, min_amount: int):
+def numbers_shares_to_buy(stocks: list[Stock], total_value: float, amount: int, min_amount: int):
     for stock in stocks:
         if stock.price > amount:
             continue
@@ -36,15 +36,17 @@ def _numbers_shares_to_buy(stocks: list[Stock], total_value: float, amount: int,
     
     print("Leftover: ", math.floor(amount))
 
+class Balancer:
 
-def main(args):
-    stocks: list[Stock] = FileLoader.load_file(args.portfolio_file)
-    stocks = API.load_shares(stocks=stocks)
-    total_value = _total_value(stocks)
-    stocks = _real_distribution(stocks, total_value+args.amount)
-    stocks = _stocks_ranking(stocks)
-    _numbers_shares_to_buy(stocks, total_value, args.amount, args.min_amount)
-    return 0
+    @staticmethod
+    def balance(args):
+        stocks: list[Stock] = FileLoader.load_file(args.portfolio_file)
+        stocks = API.load_shares(stocks=stocks)
+        total_value = total_value(stocks)
+        stocks = real_distribution(stocks, total_value+args.amount)
+        stocks = stocks_ranking(stocks)
+        numbers_shares_to_buy(stocks, total_value, args.amount, args.min_amount)
+        return 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Balance portfolio')
@@ -53,4 +55,4 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--min_amount', type=int, required=False, default=100, help='Minimum amount that need to be invested per line')
     parser.add_argument('-fs', '--full_share', action='store_true', help='Only buy full share')
     args = parser.parse_args()
-    main(args)
+    Balancer.balance(args)
