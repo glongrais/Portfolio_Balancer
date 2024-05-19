f96c0d93c6d8c2829051eca84d306943a504084d
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-05-19 22:27:47 +0200

    Added title to the tkinter window

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from portfolio_balancer.balancer import Balancer
+from table import Table
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.parent.title("Portfolio Hub")
+        table = Table(self.parent)
         Balancer.balance("No file", 500, 100)
 
 if __name__ == "__main__":
