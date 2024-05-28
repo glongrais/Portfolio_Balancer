12cdf03e428e67ed1090d0bc4b91f558cf9e5960
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-05-28 23:12:38 +0200

    load_number using sqlite

--- portfolio_database/portfolio_loader.py
+++ portfolio_database/portfolio_loader.py
@@ -1,9 +1,35 @@
 from numbers_parser import Document
 from portfolio_balancer.stock import Stock
-from pyspark.sql import SparkSession
 import pandas as pd
+import sqlite3
 
-def load_numbers(filename: str) -> list[Stock]:
+def init_table():
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS portfolio (
+                   symbol TEXT PRIMARY KEY,
+                   name TEXT,
+                   price FLOAT,
+                   quantity INTEGER,
+                   distribution_target FLOAT,
+                   distribution_real FLOAT
+
+    )
+    ''')
+    connection.commit()
+    connection.close()
+
+def insert_stock(stock: Stock):
+    with sqlite3.connect("data.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+        UPSERT INTO portfolio (symbol, quantity, distribution_target) VALUES (?, ?, ?)
+        ''', (stock.symbol, stock.quantity, stock.distribution_target))
+        connection.commit()
+
+
+def load_numbers(filename: str):
 
     # Value to adapt 
     SHEET = 'Dividends'
@@ -20,37 +46,16 @@ def load_numbers(filename: str) -> list[Stock]:
     table = doc.sheets[SHEET].tables[TABLE]
     table.delete_row(num_rows=table.num_header_rows, start_row=0)
 
-    datas = []
-    for row in table.rows(values_only=True):
-        if row[0] is None:
-            continue
-        datas.append(Stock(symbol=row[SYMBOL], quantity=int(row[QUANTITY]), distribution_target=row[DISTRIBUTION_TARGET]))
+    with sqlite3.connect("data.db") as connection:
+        cursor = connection.cursor()
+        for row in table.rows(values_only=True):
+            if row[0] is None:
+                continue
+            cursor.execute('''
+                           INSERT INTO portfolio (symbol, quantity, distribution_target) VALUES (?, ?, ?)
+                           ON CONFLICT(symbol) DO UPDATE SET quantity=excluded.quantity,  distribution_target=excluded. distribution_target 
+            ''', (row[SYMBOL], int(row[QUANTITY]), row[DISTRIBUTION_TARGET]))
 
-    df =  pd.DataFrame([data.__dict__ for data in datas])
-    df['date'] = pd.Timestamp.today()#.strftime('%Y-%m-%d')
-
-    return df
-
-def create_spark_session():
-    return SparkSession.builder \
-        .appName("Hudi Data Lake") \
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-        .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension") \
-        .config("spark.jars.packages", "org.apache.hudi:hudi-spark3.4-bundle_2.12:0.14.1") \
-        .getOrCreate()
-
-def write_data_to_hudi(data_frame, hudi_table_path):
-    data_frame.write.format("hudi") \
-        .option("hoodie.table.name", "portfolio_data") \
-        .option("hoodie.datasource.write.recordkey.field", "symbol") \
-        .option("hoodie.datasource.write.precombine.field", "date") \
-        .option("hoodie.datasource.write.operation", "upsert") \
-        .option("hoodie.datasource.write.table.name", "portfolio_data") \
-        .option("path", hudi_table_path) \
-        .mode("append") \
-        .save()
-
+cursor = init_table()
 portfolio = load_numbers("/Users/guillaumelongrais/Library/Mobile Documents/com~apple~Numbers/Documents/Investissement.numbers")
-spark = create_spark_session()
-spark_df = spark.createDataFrame(portfolio)
-write_data_to_hudi(spark_df, "/Users/guillaumelongrais/Documents/Code/Python/Portfolio_Balancing/portfolio_data")
+
