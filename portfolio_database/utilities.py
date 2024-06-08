105fe169ba3ba0c913641ac564e059f95a6af196
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-06-08 09:37:42 +0200

    Using sqlite instead of spark

--- portfolio_database/utilities.py
+++ portfolio_database/utilities.py
@@ -4,14 +4,38 @@ def init_tables():
     connection = sqlite3.connect("data.db")
     cursor = connection.cursor()
     cursor.execute('''
+    CREATE TABLE IF NOT EXISTS stocks (
+                    stockid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    symbol TEXT
+    )
+    ''')
+    cursor.execute('''
     CREATE TABLE IF NOT EXISTS portfolio (
-                   symbol TEXT PRIMARY KEY,
-                   name TEXT,
-                   price FLOAT,
-                   quantity INTEGER,
-                   distribution_target FLOAT,
-                   distribution_real FLOAT
-
+                    stockid INTEGER PRIMARY KEY,
+                    quantity INTEGER,
+                    distribution_target REAL,
+                    distribution_real REAL,
+                    FOREIGN KEY (stockid) REFERENCES stocks (stockid)
+    )
+    ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS historicalstocks (
+                    closeprice REAL    NULL    ,
+                    stockid    INTEGER NOT NULL,
+                    datestamp  TEXT    NULL    ,
+                    FOREIGN KEY (stockid) REFERENCES stocks (stockid)
+    )
+    ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS transactions (
+                    transactionid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    stockid       INTEGER NOT NULL,
+                    quantity      INTEGER NULL    ,
+                    price         REAL    NOT NULL,
+                    type          TEXT    NULL    ,
+                    datestamp     TEXT    NULL    ,
+                    FOREIGN KEY (stockid) REFERENCES stocks (stockid)
     )
     ''')
     connection.commit()
