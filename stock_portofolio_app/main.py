7dab98c6139771a1b9b4b1f53b57b1a203945bbd
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-06-11 17:20:32 +0200

    entrypoint from Numbers

--- stock_portofolio_app/main.py
+++ stock_portofolio_app/main.py
@@ -1,7 +1,9 @@
 from services.portfolio_service import PortfolioService
 from models.Stock import Stock
+from utils.file_utils import load_numbers
 
 if __name__ == '__main__':
     Stock()
+    load_numbers("/Users/guillaumelongrais/Library/Mobile Documents/com~apple~Numbers/Documents/Investissement.numbers")
     c = PortfolioService()
     print(c.calculate_portfolio_value())
