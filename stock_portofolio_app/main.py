01f4bd51531c8861bd87968db6dbec1c6ad25161
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-06-11 22:56:41 +0200

    main update

--- stock_portofolio_app/main.py
+++ stock_portofolio_app/main.py
@@ -3,7 +3,7 @@ from models.Stock import Stock
 from utils.file_utils import load_numbers
 
 if __name__ == '__main__':
-    Stock()
+    S = Stock()
+    S.update_prices()
     load_numbers("/Users/guillaumelongrais/Library/Mobile Documents/com~apple~Numbers/Documents/Investissement.numbers")
-    c = PortfolioService()
-    print(c.calculate_portfolio_value())
+    print(PortfolioService().calculate_portfolio_value())
