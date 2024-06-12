e3ea73d16b0faf02db39b155a18ef7093114742b
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-06-12 17:17:50 +0200

    portfolio balancer from db

--- stock_portofolio_app/services/portfolio_service.py
+++ stock_portofolio_app/services/portfolio_service.py
@@ -1,28 +1,27 @@
 # services/portfolio_service.py
 from models.Portfolio import Portfolio
 from models.Stock import Stock
-#from models.transaction import Transaction
+from models.Transaction import Transaction
 #from models.historical_stock import HistoricalStock
 #from external.stock_price_api import StockPriceAPI
 from external.historical_data_api import HistoricalDataAPI
+import time
+import math
+from functools import lru_cache
 
 class PortfolioService:
-    def __init__(self):
-        self.portfolio = Portfolio()
-        self.stock = Stock()
-        #self.transaction = Transaction()
-        #self.historical_stock = HistoricalStock()
-        #self.stock_price_api = StockPriceAPI(api_key='YOUR_API_KEY')
-        self.historical_data_api = HistoricalDataAPI()
 
-    def calculate_portfolio_value(self) -> float:
+    @classmethod
+    @lru_cache()
+    def calculate_portfolio_value(cls, ttl_hash=round(time.time() / 60)) -> float:
         """
         Calculates the total value of the portfolio.
 
         Returns:
         - float: Total portfolio value
         """
-        portfolio_entries = self.portfolio.execute_query(
+        portfolio = Portfolio()
+        portfolio_entries = portfolio.execute_query(
             '''
             SELECT stocks.stockid, portfolio.quantity, stocks.price FROM portfolio LEFT JOIN stocks ON portfolio.stockid = stocks.stockid 
             '''
@@ -31,13 +30,58 @@ class PortfolioService:
             return 0
         else:
             total_value = sum(quantity * price for stockid, quantity, price in portfolio_entries)
-            return total_value
+            return round(total_value)
 
-    def get_transaction_history(self) -> list:
+    @classmethod
+    def balance_portfolio(cls, amount_to_buy, min_amount_to_buy=100):
+        total_value = cls.calculate_portfolio_value()
+
+        cls.update_real_distribution()
+
+        portfolio = Portfolio()
+        stocks = portfolio.execute_query(
+            '''
+                SELECT stocks.stockid, stocks.symbol, stocks.price, (portfolio.distribution_target - portfolio.distribution_real) as delta FROM portfolio LEFT JOIN stocks ON portfolio.stockid = stocks.stockid ORDER BY delta DESC
+            '''
+        )
+        for stockid, symbol, price, delta in stocks:
+            if price > amount_to_buy:
+                continue
+            target = delta/100
+            money_to_buy = target * (total_value+amount_to_buy)
+            tmp = math.floor(min(amount_to_buy, money_to_buy)/price)
+            if (tmp*price) < min_amount_to_buy:
+                continue
+            amount_to_buy = amount_to_buy - (tmp*price)
+            print(symbol, tmp, tmp*price, " Stock price: ", price)
+        
+        print("Leftover: ", math.floor(amount_to_buy))
+
+    @classmethod
+    def update_real_distribution(cls):
+        total_value = cls.calculate_portfolio_value()
+
+        portfolio = Portfolio()
+        portfolio_entries = portfolio.execute_query(
+            '''
+            SELECT stocks.stockid, portfolio.quantity, stocks.price FROM portfolio LEFT JOIN stocks ON portfolio.stockid = stocks.stockid 
+            '''
+        )
+
+        if portfolio_entries == None:
+            return
+        else:
+            for stockid, quantity, price in portfolio_entries:
+                distribution_real = round((price*quantity)/total_value*100, 2)
+                portfolio.execute_query('UPDATE portfolio SET distribution_real = ? WHERE stockid = ?', (distribution_real, stockid,))
+
+    @classmethod
+    def get_transaction_history(cls) -> list:
         """
         Retrieves the transaction history.
 
         Returns:
         - list: Transaction history
         """
-        return self.transaction.fetchall('SELECT * FROM transactions')
+        transaction = Transaction()
+        return transaction.fetchall('SELECT * FROM transactions')
