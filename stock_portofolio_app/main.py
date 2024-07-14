159a8f47a2f802eda50598f3cb6a3c288e4e1d4a
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-07-14 23:02:59 +0200

    Added portofolio as a dataclass

--- stock_portofolio_app/main.py
+++ stock_portofolio_app/main.py
@@ -1,10 +1,14 @@
+import logging
 from services.portfolio_service import PortfolioService
 from services.data_processing import DataProcessing
 from models.Stock import Stock
 from utils.file_utils import load_numbers
 from services.database_service import DatabaseService
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(name)s - %(message)s')
     # S = Stock()
     # S.update_prices()
     # load_numbers("/Users/guillaumelongrais/Library/Mobile Documents/com~apple~Numbers/Documents/Investissement.numbers")
@@ -12,4 +16,5 @@ if __name__ == '__main__':
     # DataProcessing.fetch_historical_dividends(["TTE.PA"])
     # PortfolioService.balance_portfolio(3000)
     DatabaseService.getStocks()
-    print(DatabaseService.stocks)
+    print(DatabaseService.getPortfolios())
+
