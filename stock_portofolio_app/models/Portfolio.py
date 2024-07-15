d1ebc05ce5712864b7309130cfd6b0bd2a6ca7dc
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-07-15 23:30:26 +0200

    Added portfolio database service + tests

--- stock_portofolio_app/models/Portfolio.py
+++ stock_portofolio_app/models/Portfolio.py
@@ -3,40 +3,40 @@ from enum import Enum
 from dataclasses import dataclass, field
 from models.Stock import Stock
 
-class Portfolio(BaseModel):
+# class Portfolio(BaseModel):
 
-    class Field(Enum):
-        STOCKID = 'stockid'
-        QUANTITY = 'quantity'
-        DISTRIBUTIONTARGET = 'distribution_target'
-        DISTRIBUTIONREAL = 'distribution_real'
+#     class Field(Enum):
+#         STOCKID = 'stockid'
+#         QUANTITY = 'quantity'
+#         DISTRIBUTIONTARGET = 'distribution_target'
+#         DISTRIBUTIONREAL = 'distribution_real'
     
-    def __init__(self, db_path='data/portfolio.db'):
-        super().__init__('portfolio', db_path)
-        self.create_table()
+#     def __init__(self, db_path='data/portfolio.db'):
+#         super().__init__('portfolio', db_path)
+#         self.create_table()
 
-    def create_table(self):
-        self.execute_query('''
-        CREATE TABLE IF NOT EXISTS portfolio (
-            stockid INTEGER PRIMARY KEY,
-            quantity INTEGER NOT NULL,
-            distribution_target REAL,
-            distribution_real REAL,
-            FOREIGN KEY (stockid) REFERENCES stocks(stockid)
-        )
-        ''')
+#     def create_table(self):
+#         self.execute_query('''
+#         CREATE TABLE IF NOT EXISTS portfolio (
+#             stockid INTEGER PRIMARY KEY,
+#             quantity INTEGER NOT NULL,
+#             distribution_target REAL,
+#             distribution_real REAL,
+#             FOREIGN KEY (stockid) REFERENCES stocks(stockid)
+#         )
+#         ''')
 
-    def add_to_portfolio(self, stockid, quantity, distribution_target):
-        self.execute_query('''
-            INSERT INTO portfolio (stockid, quantity, distribution_target) VALUES (?, ?, ?)
-            ON CONFLICT(stockid) DO UPDATE SET quantity=excluded.quantity,  distribution_target=excluded.distribution_target 
-        ''', (stockid, quantity, distribution_target))
+#     def add_to_portfolio(self, stockid, quantity, distribution_target):
+#         self.execute_query('''
+#             INSERT INTO portfolio (stockid, quantity, distribution_target) VALUES (?, ?, ?)
+#             ON CONFLICT(stockid) DO UPDATE SET quantity=excluded.quantity,  distribution_target=excluded.distribution_target 
+#         ''', (stockid, quantity, distribution_target))
 
-    def update_field(self, stockid, value, field: Field):
-        self.execute_query('''
-        UPDATE portfolio SET ? = ?
-        WHERE stockid = ?
-        ''', (field, value, stockid))
+#     def update_field(self, stockid, value, field: Field):
+#         self.execute_query('''
+#         UPDATE portfolio SET ? = ?
+#         WHERE stockid = ?
+#         ''', (field, value, stockid))
 
 @dataclass
 class Portfolio:
