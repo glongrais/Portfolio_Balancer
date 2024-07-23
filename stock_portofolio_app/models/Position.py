ee1c0a75e91e23c40ca7df0f44bdc42be62598aa
Guillaume Longrais <guillaume.longrais@orange.fr>
2024-07-23 21:42:47 +0200

    balancePortfolio with dataclass

--- stock_portofolio_app/models/Position.py
+++ stock_portofolio_app/models/Position.py
@@ -2,49 +2,18 @@ from models.Base import BaseModel
 from enum import Enum
 from dataclasses import dataclass, field
 from models.Stock import Stock
-
-# class Position(BaseModel):
-
-#     class Field(Enum):
-#         STOCKID = 'stockid'
-#         QUANTITY = 'quantity'
-#         DISTRIBUTIONTARGET = 'distribution_target'
-#         DISTRIBUTIONREAL = 'distribution_real'
-    
-#     def __init__(self, db_path='data/portfolio.db'):
-#         super().__init__('Position', db_path)
-#         self.create_table()
-
-#     def create_table(self):
-#         self.execute_query('''
-#         CREATE TABLE IF NOT EXISTS Position (
-#             stockid INTEGER PRIMARY KEY,
-#             quantity INTEGER NOT NULL,
-#             distribution_target REAL,
-#             distribution_real REAL,
-#             FOREIGN KEY (stockid) REFERENCES stocks(stockid)
-#         )
-#         ''')
-
-#     def add_to_Position(self, stockid, quantity, distribution_target):
-#         self.execute_query('''
-#             INSERT INTO Position (stockid, quantity, distribution_target) VALUES (?, ?, ?)
-#             ON CONFLICT(stockid) DO UPDATE SET quantity=excluded.quantity,  distribution_target=excluded.distribution_target 
-#         ''', (stockid, quantity, distribution_target))
-
-#     def update_field(self, stockid, value, field: Field):
-#         self.execute_query('''
-#         UPDATE Position SET ? = ?
-#         WHERE stockid = ?
-#         ''', (field, value, stockid))
+from typing import Optional
 
 @dataclass
 class Position:
     stockid: int
     quantity: int
-    distribution_target: float = field(default=None)
+    distribution_target: Optional[float] = field(default=None)
     distribution_real: float = field(default=0.0)
-    stock: Stock = field(default=None)
+    stock: Optional['Stock'] = field(default=None)
+
+    def delta(self):
+        return (self.distribution_target or 0.0) - self.distribution_real
 
     @classmethod
     def dataclass_factory(cls, cursor, row):
