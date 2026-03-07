1b1060d8bc54e4707ad71dd67875065010dc4c2e
Guillaume Longrais <guillaume.longrais@orange.fr>
2026-03-07 18:59:57 +0100

    Fix /stocks endpoint

--- dbt/stock_portfolio_dbt/models/core/core__stocks.sql
+++ dbt/stock_portfolio_dbt/models/core/core__stocks.sql
@@ -1,6 +1,24 @@
 -- stock_portfolio_dbt/models/core/core__stocks.sql
 WITH
-stocks AS (SELECT * FROM {{ ref('stg__stocks') }}),
+stocks AS (
+    SELECT
+        stockid,
+        name,
+        symbol,
+        price,
+        currency,
+        market_cap,
+        sector,
+        industry,
+        country,
+        logo_url,
+        quote_type,
+        ex_dividend_date,
+        previous_close,
+        dividend,
+        dividend_yield
+    FROM {{ ref('stg__stocks') }}
+),
 unlisted_stocks AS (SELECT * FROM {{ ref('stg__unlisted_stocks') }})
 
 SELECT
@@ -16,5 +34,11 @@ SELECT
     NULL AS market_cap,
     NULL AS sector,
     NULL AS industry,
-    NULL AS country
+    NULL AS country,
+    '' AS logo_url,
+    'EQUITY' AS quote_type,
+    NULL AS ex_dividend_date,
+    0 AS previous_close,
+    0 AS dividend,
+    0 AS dividend_yield
 FROM unlisted_stocks
--- stock_portfolio_app/utils/db_utils.py
+++ stock_portfolio_app/utils/db_utils.py
@@ -322,17 +322,44 @@ def _migrate_dbt_views(cursor):
     cursor.execute('''
     CREATE VIEW IF NOT EXISTS core__stocks AS
     WITH
-        stocks AS (SELECT * FROM stg__stocks),
+        stocks AS (
+            SELECT
+                stockid,
+                name,
+                symbol,
+                price,
+                currency,
+                market_cap,
+                sector,
+                industry,
+                country,
+                logo_url,
+                quote_type,
+                ex_dividend_date,
+                previous_close,
+                dividend,
+                dividend_yield
+            FROM stg__stocks
+        ),
         unlisted_stocks AS (SELECT * FROM stg__unlisted_stocks)
     SELECT * FROM stocks
     UNION ALL
     SELECT
-        stockid, name, symbol,
-        NULL AS price, NULL AS currency, NULL AS market_cap,
-        NULL AS sector, NULL AS industry, NULL AS country,
-        0 AS previous_close, 0 AS dividend, 0 AS dividend_yield,
-        '' AS logo_url, 'EQUITY' AS quote_type,
-        NULL AS ex_dividend_date
+        stockid,
+        name,
+        symbol,
+        NULL AS price,
+        NULL AS currency,
+        NULL AS market_cap,
+        NULL AS sector,
+        NULL AS industry,
+        NULL AS country,
+        '' AS logo_url,
+        'EQUITY' AS quote_type,
+        NULL AS ex_dividend_date,
+        0 AS previous_close,
+        0 AS dividend,
+        0 AS dividend_yield
     FROM unlisted_stocks
     ''')
 
